c880299d9a4bed7f7d1e6c5e43cc127e2080f067
Mahsa <mahsahaddadi19@gmail.com>
2024-02-28 12:02:11 +1100

    Design Profile Page

--- src/components/FeedPosts/PostFooter.jsx
+++ src/components/FeedPosts/PostFooter.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { CommentLogo, NotificationsLogo, UnlikeLogo } from "../../assets/constants";
 
 
-const PostFooter = ({username}) => {
+const PostFooter = ({ username, isProfilePage }) => {
 
     const [liked, setLiked] = useState(false);
     const [likes, setLikes] = useState(1000);
@@ -21,7 +21,7 @@ const PostFooter = ({username}) => {
 
 
   return (
-    <Box mb={10}>
+    <Box mb={10} marginTop={"auto"}>
         <Flex alignItems={"center"} gap={4} w={"full"} pt={0} mb={2} mt={4}>
         <Box onClick={handleLike} cursor={"pointer"} fontSize={18}>
             {!liked ? <NotificationsLogo /> : <UnlikeLogo /> }
@@ -36,15 +36,19 @@ const PostFooter = ({username}) => {
             {likes} likes
         </Text>
 
-        <Text fontSize="sm" fontWeight={700}>
-            {username}{" "}
-            <Text as="span" fontWeight={400}>
-                Feeling good
-            </Text>
-        </Text>
-        <Text fontSize="sm" color={"gray"}>
-            View all 1,000 comments
-        </Text>
+        {!isProfilePage && (
+            <>
+                <Text fontSize="sm" fontWeight={700}>
+                    {username}{" "}
+                    <Text as="span" fontWeight={400}>
+                        Feeling good
+                    </Text>
+                </Text>
+                <Text fontSize="sm" color={"gray"}>
+                    View all 1,000 comments
+                </Text>
+            </>
+        )}
         
 
         <Flex alignItems={"center"} gap={2} justifyContent={"space-between"} w={"full"}>
