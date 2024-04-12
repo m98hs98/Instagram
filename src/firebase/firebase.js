ddd25c1143531163611054b922e7e3af0717fb8d
Mahsa <mahsahaddadi19@gmail.com>
2024-04-12 15:34:22 +1000

    New Firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,14 +4,24 @@ import {getFirestore} from "firebase/firestore";
 import {getStorage} from "firebase/storage";
 
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCo5YwbTXe5vIfuD2ijJfdgcQpgyfyw7Vg",
+//   authDomain: "instagram-clone-1518c.firebaseapp.com",
+//   projectId: "instagram-clone-1518c",
+//   storageBucket: "instagram-clone-1518c.appspot.com",
+//   messagingSenderId: "746835937508",
+//   appId: "1:746835937508:web:e2d340ded858e341ed04f1",
+//   measurementId: "G-V2K9KQFWG9"
+// };
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCo5YwbTXe5vIfuD2ijJfdgcQpgyfyw7Vg",
-  authDomain: "instagram-clone-1518c.firebaseapp.com",
-  projectId: "instagram-clone-1518c",
-  storageBucket: "instagram-clone-1518c.appspot.com",
-  messagingSenderId: "746835937508",
-  appId: "1:746835937508:web:e2d340ded858e341ed04f1",
-  measurementId: "G-V2K9KQFWG9"
+  apiKey: "AIzaSyBdLf5kVHHEla049Ty_h2OE09Qpc3WFXYk",
+  authDomain: "instagram-dev-mahsa.firebaseapp.com",
+  projectId: "instagram-dev-mahsa",
+  storageBucket: "instagram-dev-mahsa.appspot.com",
+  messagingSenderId: "926164822839",
+  appId: "1:926164822839:web:6f4d9cb40a43fd1e2d48b2"
 };
 
 const app = initializeApp(firebaseConfig);
